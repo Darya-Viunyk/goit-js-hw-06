aa1ddacfe2f36fb82de2123c1a1e5096e8d75524
Darya-Viunyk <dar.viunyk@gmail.com>
2022-09-13 23:27:16 +0300

    task-2

--- src/src/js/task-02.js
+++ src/src/js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const mainCategories = document.querySelector("ul#ingredients");
+ingredients.forEach((element) => {
+  const li = document.createElement("li");
+  li.textContent = element;
+  li.classList.add("item");
+  mainCategories.appendChild(li);
+});
+
