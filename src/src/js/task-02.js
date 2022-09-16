44aec4e7c46c8f9bb384cb75e8454dffd57c402d
Darya-Viunyk <dar.viunyk@gmail.com>
2022-09-16 22:25:26 +0300

    fixup-task-02

--- src/src/js/task-02.js
+++ src/src/js/task-02.js
@@ -7,14 +7,10 @@ const ingredients = [
   'Condiments',
 ];
 
-
-
  const mainCategories = document.querySelector("ul#ingredients");
-let renderedHtml = '';
 ingredients.forEach((element) => {
   const li = document.createElement("li");
   li.textContent = element;
   li.classList.add("item");
   mainCategories.append(li);
 });
-
