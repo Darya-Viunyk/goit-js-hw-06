985c52bee1a7af982bb4dca67c62692fd8768366
Darya-Viunyk <dar.viunyk@gmail.com>
2022-09-21 16:52:15 +0300

    task-10

--- src/src/js/task-10.js
+++ src/src/js/task-10.js
@@ -1,3 +1,32 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+function createBoxes(amount) {
+  for (let i = 0; i < amount; i++) {
+    const size = 30 + boxesAmount * 10;
+    const item = document.createElement('div');
+    item.style.width = `${size}px`;
+    item.style.height = `${size}px`;
+    item.style.backgroundColor = getRandomHexColor();
+    boxes.append(item);
+    boxesAmount++;
+  }
+}
+
+function destroyBoxes () {
+  boxes.textContent = '';
+  boxesAmount = 0;
+}
+
+let boxesAmount = 0;
+const boxes = document.querySelector('#boxes');
+// Add boxes.
+document.querySelector('#controls [data-create=""]').addEventListener('click', function () {
+  const value = document.querySelector('#controls input').value;
+  if (value) {
+    createBoxes(parseInt(value));
+  }
+});
+// Clear boxes.
+document.querySelector('#controls [data-destroy=""]').addEventListener('click', destroyBoxes);
