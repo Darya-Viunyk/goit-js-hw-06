6a0ec96230c90159a46a5fa70de4a83b04955352
Darya-Viunyk <dar.viunyk@gmail.com>
2022-09-21 16:51:39 +0300

    task-8

--- src/src/js/task-08.js
+++ src/src/js/task-08.js
@@ -1,1 +1,29 @@
 
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', submitForm);
+
+function submitForm(e) {
+    // Stop the page reload.
+    e.preventDefault();
+
+    let result = {};
+    const elements  = e.target.elements;
+    for (let i = 0; i < elements.length; i++) {
+        // Check only input fields.
+        if (elements[i].nodeName !== 'INPUT') {
+            continue;
+        }
+        // Validate field.
+        if (!elements[i].value) {
+            alert(`The field ${elements[i].name} is required one, please setup it please.`);
+            return;
+        }
+        // Collect data.
+        result[elements[i].name] = elements[i].value;
+    }
+    // Print result.
+    console.log(result);
+    // Reset form.
+    e.target.reset();
+}
