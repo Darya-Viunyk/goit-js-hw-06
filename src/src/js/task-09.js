f3a72b5af666a094553aef3ea43c7adb75abbc97
Darya-Viunyk <dar.viunyk@gmail.com>
2022-09-21 16:51:56 +0300

    task-9

--- src/src/js/task-09.js
+++ src/src/js/task-09.js
@@ -1,3 +1,14 @@
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+document.querySelector('.change-color').addEventListener('click', updateColor);
+const colorOutput = document.querySelector('.color');
+const body = document.querySelector('body');
+
+function updateColor(e) {
+  const newColor = getRandomHexColor();
+  colorOutput.textContent = newColor;
+  body.setAttribute('style', `background-color: ${newColor}`);
 }
